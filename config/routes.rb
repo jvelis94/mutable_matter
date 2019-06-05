799ff700a0fa1db08ca2c9ca1d7ba187974cacdd
Joel Velis <joelvelis94@gmail.com>
2019-06-05 21:45:41 +0200

    generate necessary controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#index"
+  root to: "pages#home"
   devise_for :users, controllers: { registrations: "registrations", sessions: "sessions" }
   resources :posts
   
