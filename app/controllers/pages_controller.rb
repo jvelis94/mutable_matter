5c1cf4dd3bcc04da07086024a567ac60b8b2068f
Joel Velis <joelvelis94@gmail.com>
2019-06-12 18:30:04 -0400

    progress on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,21 @@
 class PagesController < ApplicationController
     # skip_before_action :authenticate_user!, only: :home
+    before_action :find_id, only: [:show]
+
 
   def home
+    @posts = Post.all
   end
 
+  def show
+  end
+  
   def components
   end
+
+  private
+
+    def find_id
+        @post = Post.find(params[:id])
+    end
 end
