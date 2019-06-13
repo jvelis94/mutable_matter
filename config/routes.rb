e326e894c487b3a810e1c6a362ae6444c1132ea4
Joel Velis <joelvelis94@gmail.com>
2019-06-13 08:51:34 -0400

    uncomment devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  # devise_for :users, controllers: { registrations: "registrations", sessions: "sessions" }
+  devise_for :users, controllers: { registrations: "registrations", sessions: "sessions" }
   resources :posts
   
 end
