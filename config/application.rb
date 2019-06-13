6a9b52e42f3fc9824fce3021a4c2605f1d69e44b
Joel Velis <joelvelis94@gmail.com>
2019-06-13 09:34:36 -0400

    heroku

--- config/application.rb
+++ config/application.rb
@@ -8,14 +8,19 @@ Bundler.require(*Rails.groups)
 
 module MutableMatter
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
+# Initialize configuration defaults for originally generated Rails version.
+config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
   end
 end
 
