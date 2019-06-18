fe565e24f1bcfc2ff0cee58adfac1841d9084b3d
Joel Velis <joelvelis94@gmail.com>
2019-06-18 14:34:18 -0400

    devise configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments, through: :posts
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
 end
