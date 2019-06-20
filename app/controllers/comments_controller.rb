27da4a65a92b561c2b20eb7a9e907fa668b602dd
Joel Velis <joelvelis94@gmail.com>
2019-06-19 20:44:43 -0400

    create comments controller methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+    def index
+        @comments = Comment.all
+    end
+
+    def new
+        @comment = Comment.new
+    end
+    
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.user = current_user
+        @comment.post = current_user.post
+        respond_to do |format|
+            if @comment.save
+                format.html { redirect_to post_path, notice: 'Post was successfully created.' }
+            else
+                render action: 'new'
+            end
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require('comment').permit(:description)
+    end
 end
