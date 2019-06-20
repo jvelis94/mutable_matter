290330b12826b9f3913eb211ca11a5a064f9d2d9
Joel Velis <joelvelis94@gmail.com>
2019-06-20 09:56:12 -0400

    set up authentification to access certain pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:index]
+    skip_before_action :authenticate_user!, only: [:index, :show]
     before_action :find_id, only: [:show, :edit, :update, :destroy]
 
     def index
