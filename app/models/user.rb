276023dbb5ec563c7fcaa3e445094e20a36b3a48
Joel Velis <joelvelis94@gmail.com>
2019-06-21 14:10:40 -0400

    uncommit confirmable in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable 
-        #  :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts
   has_many :comments, through: :posts
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
