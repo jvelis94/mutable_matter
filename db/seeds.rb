63353b9cf08af65c671b01eb64b72cbfe90e00c9
Joel Velis <joelvelis94@gmail.com>
2019-06-22 11:53:41 -0400

    add default avatar image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,97 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+# User.destroy_all
+# Post.destroy_all
+# Comment.destroy_all
+
+
+
+
+
+# puts "Creating users"
+
+# user1 = User.create!(
+#   email: Faker::Internet.email, 
+#   password: "password", 
+#   first_name: Faker::Name.male_first_name , 
+#   last_name: Faker::Name.last_name, 
+#   role: "Farmer",
+#   remote_avatar_url:'https://images.unsplash.com/photo-1528695037290-720b9ad2f242?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+#     )
+
+# farmer2 = User.create!(
+#   email: Faker::Internet.email, 
+#   password: "password", 
+#   first_name: Faker::Name.male_first_name , 
+#   last_name: Faker::Name.last_name, 
+#   role: "Farmer",
+#   remote_avatar_url:'https://images.unsplash.com/photo-1529600490622-6fe4b898c84e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+#    )
+
+# farmer3 = User.create!(
+# email: Faker::Internet.email, 
+# password: "password", 
+# first_name: Faker::Name.male_first_name , 
+# last_name: Faker::Name.last_name, 
+# role: "Farmer",
+# remote_avatar_url:'https://images.unsplash.com/photo-1509506489701-dfe23b067808?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+# )
+  
+# farmer4 = User.create!(
+#   email: Faker::Internet.email, 
+#   password: "password", 
+#   first_name: Faker::Name.male_first_name , 
+#   last_name: Faker::Name.last_name, 
+#   role: "Farmer",
+#   remote_avatar_url:'https://images.unsplash.com/photo-1558284811-bebfac1d15e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+#   )
+
+      
+
+
+#         puts "Creating users"
+
+#         resto1 = User.create!(
+#           email: Faker::Internet.email,
+#           password: "password",
+#           first_name: Faker::Name.male_first_name ,
+#           last_name: Faker::Name.last_name,
+#           role: "Restaurant",
+#           remote_avatar_url: "https://images.unsplash.com/photo-1559548331-f9cb98001426?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+#             )
+         
+#          resto2 = User.create!(
+#           email: Faker::Internet.email,
+#           password: "password",
+#           first_name: Faker::Name.male_first_name ,
+#           last_name: Faker::Name.last_name,
+#           role: "Restaurant",
+#           remote_avatar_url: 'https://images.unsplash.com/photo-1553484771-689277e6fa16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+#             )
+         
+#          resto3 = User.create!(
+#           email: Faker::Internet.email,
+#           password: "password",
+#           first_name: Faker::Name.male_first_name ,
+#           last_name: Faker::Name.last_name,
+#           role: "Restaurant",
+#           remote_avatar_url: 'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+#             )
+         
+#          resto4 = User.create!(
+#           email: Faker::Internet.email,
+#           password: "password",
+#           first_name: Faker::Name.male_first_name ,
+#           last_name: Faker::Name.last_name,
+#           role: "Restaurant",
+#           remote_avatar_url: 'https://images.unsplash.com/photo-1558700213-50487e1b267a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+#             )
+         
+         
+#             )
+
